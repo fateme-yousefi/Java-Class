377b0fe6a8cdb148e8cb0aafab8192fd4eadebee
fateme <fatemeyousefi9675@gmail.com>
2024-05-20 22:05:06 +0330

    Upload Hello World on Github

---
+++ App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int age = 27;
+        int birthdate = 1996;
+        int height = 12;
+        byte Min = -128;
+        byte Max = 127;
+        short age2 = 55;
+//        short Phonenumber = 9999999;
+        
+        System.out.println(age);
+    }
+}
